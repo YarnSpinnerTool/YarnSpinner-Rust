12b35c439540a636c0085325c5a7620799828341
Mathias Fischler <mathias.fischler@messerli.ch>
2023-04-18 21:05:41 +0200

    Show that header and text works

--- crates/core/examples/parse_input.rs
+++ crates/core/examples/parse_input.rs
@@ -8,6 +8,7 @@ fn main() {
 title: Node_Title
 ---
 Here are some lines!
+That's weird?
 Wow!
 ==="
         .into(),
@@ -16,6 +17,42 @@ Wow!
 
     let dialogue_context = parser.dialogue().unwrap();
     let hashtags = dialogue_context.file_hashtag_all();
+
     println!("{:?}", hashtags[0].HASHTAG_TEXT());
     println!("{:?}", hashtags[1].HASHTAG_TEXT());
+
+    let nodes = dialogue_context.node_all();
+    let first_node: &std::rc::Rc<parser_rule_context::BaseParserRuleContext<NodeContextExt>> =
+        &nodes[0];
+    let statements = first_node.body().unwrap().statement_all();
+    println!(
+        "{:?}",
+        statements[0]
+            .line_statement()
+            .unwrap()
+            .line_formatted_text()
+            .unwrap()
+            .TEXT_all()
+    );
+    println!(
+        "{:?}",
+        statements[1]
+            .line_statement()
+            .unwrap()
+            .line_formatted_text()
+            .unwrap()
+            .TEXT_all()
+    );
+    println!(
+        "{:?}",
+        statements[2]
+            .line_statement()
+            .unwrap()
+            .line_formatted_text()
+            .unwrap()
+            .TEXT_all()
+    );
+
+    let title = first_node.header(0);
+    println!("{:?}", title.unwrap().REST_OF_LINE().unwrap())
 }
