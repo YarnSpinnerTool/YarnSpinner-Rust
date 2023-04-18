7d558a60f868a3f41d5c46fcaf1b65ee71d224f6
Jan Nils Ferner <contact@jnferner.com>
2023-04-18 16:18:21 +0200

    Move executable to examples

--- crates/core/examples/parse_input.rs
+++ crates/core/examples/parse_input.rs
@@ -1,6 +1,5 @@
 use rusty_yarn_spinner::parser::parse;
 
-
 fn main() {
     let input = "# hello comment any hashtag content\ntitle: blub i am going until the end\n---\nHello Line of Text\n===\n";
     let (hashtags, dialogue) = parse(input);
