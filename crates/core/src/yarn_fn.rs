1c308f3b176364b0ee81ef74bbdf2c87eed80f65
Jan Hohenheim <jan@hohenheim.ch>
2023-04-20 03:11:44 +0200

    Fix typo

--- crates/core/src/yarn_fn.rs
+++ crates/core/src/yarn_fn.rs
@@ -2,7 +2,7 @@
 //! Inspired by how Bevy stores [`FnSystem`](https://docs.rs/bevy_ecs/0.10.1/bevy_ecs/system/struct.FnSystem.html)s.
 //! This is all here just to emulate the `Dictionary<string, Delegate>` used in Yarn Spinner's `Library` class.
 
-mod function_regristry;
+mod function_registry;
 mod function_wrapping;
 
-pub use {function_regristry::*, function_wrapping::*};
+pub use {function_registry::*, function_wrapping::*};
