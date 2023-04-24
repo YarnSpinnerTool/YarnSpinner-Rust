ff1240aa860d296916d5195fcfc1a2374569723e
Jan Hohenheim <jan@hohenheim.ch>
2023-04-24 15:07:19 +0200

    Implement part of LastLineBeforeOptionsVisitor

--- crates/compiler/src/visitors/last_line_before_options_visitor.rs
+++ crates/compiler/src/visitors/last_line_before_options_visitor.rs
@@ -1,9 +1,12 @@
 //! Adapted from <https://github.com/YarnSpinnerTool/YarnSpinner/blob/da39c7195107d8211f21c263e4084f773b84eaff/YarnSpinner.Compiler/LastLineBeforeOptionsVisitor.cs>
 
 use crate::compiler;
-use crate::parser::generated::yarnspinnerparser::{BodyContext, Line_statementContext};
+use crate::parser::generated::yarnspinnerparser::{
+    BodyContext, If_statementContext, Line_statementContext,
+};
 use crate::prelude::generated::yarnspinnerparser::{
-    BodyContextAttrs, StatementContextAll, StatementContextAttrs, YarnSpinnerParserContextType,
+    BodyContextAttrs, Else_clauseContextAttrs, If_clauseContextAttrs, If_statementContextAttrs,
+    StatementContextAll, StatementContextAttrs, YarnSpinnerParserContextType,
 };
 use crate::prelude::generated::yarnspinnerparservisitor::YarnSpinnerParserVisitorCompat;
 use antlr_rust::tree::ParseTreeVisitorCompat;
@@ -35,7 +38,25 @@ impl<'input> YarnSpinnerParserVisitorCompat<'input> for LastLineBeforeOptionsVis
     // entry point for everything
     // if there are no ifs or options with embedded statements this will be all that is visited
     fn visit_body(&mut self, ctx: &BodyContext<'input>) -> Self::Return {
-        self.run_through_statement(ctx.statement_all());
+        self.run_through_statement(&ctx.statement_all());
+    }
+
+    // handles the statements inside of an if statement
+    // chunks its way through the if, any else-ifs and elses internal block of statements
+    fn visit_if_statement(&mut self, ctx: &If_statementContext<'input>) -> Self::Return {
+        // TODO: Check this, I was distracted
+        let statements = ctx.if_clause().unwrap().statement_all();
+        self.run_through_statement(&statements);
+        if let Some(else_ifs) = ctx.else_if_clause() {
+            for else_if in else_ifs {
+                let statements = else_if.statement_all();
+                self.run_through_statement(&statements);
+            }
+        }
+        if let Some(else_statement) = ctx.else_clause() {
+            let statements = else_statement.statement_all();
+            self.run_through_statement(&statements);
+        }
     }
 }
 
@@ -47,7 +68,7 @@ impl LastLineBeforeOptionsVisitor {
     // 3. if both of these hold true we have found a line we need to flag as being before options
     // 4. repeat this process until we run out of statements to check
     // this has the potential to have VERY deep call stacks
-    fn run_through_statement(&mut self, statements: Vec<Rc<StatementContextAll>>) {
+    fn run_through_statement(&mut self, statements: &[Rc<StatementContextAll>]) {
         for (i, statement) in statements.iter().enumerate() {
             // if we are an if-block we have to visit it in case there are options and lines inside of that
             // once that is done we can move onto the next statement
