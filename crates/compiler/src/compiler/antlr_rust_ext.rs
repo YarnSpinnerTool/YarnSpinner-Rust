0bdce6bb1e0a7f0cd2b61560cd5f9eef7e58a507
Jan Hohenheim <jan@hohenheim.ch>
2023-04-26 01:04:47 +0200

    Add source comments

--- crates/compiler/src/compiler/antlr_rust_ext.rs
+++ crates/compiler/src/compiler/antlr_rust_ext.rs
@@ -8,12 +8,35 @@ use antlr_rust::token_stream::TokenStream;
 use antlr_rust::InputStream;
 
 pub(crate) trait CommonTokenStreamExt<'input> {
+    /// Collect all tokens on specified channel to the left of
+    /// the current token up until we see a token on
+    /// [`Lexer::DefaultTokenChannel`].
+    /// If `channel` is -1, find any non default channel token.
     fn get_hidden_tokens_to_left(
         &self,
         token_index: isize,
         channel: isize,
     ) -> Vec<CommonToken<'input>>;
 
+    /// A collection of all tokens fetched from the token source.
+    ///
+    /// The list is considered a complete view of the input once
+    /// `fetchedEOF` is set to `true`.
+    fn get_tokens(&self) -> Vec<CommonToken<'input>>;
+}
+
+trait PrivateCommonTokenStreamExt<'input> {
+    /// Given a starting index, return the index of the previous token on
+    /// channel.
+    ///
+    /// Given a starting index, return the index of the previous token on
+    /// channel.
+    /// Return `token_index` if `tokens[token_index]` is on channel.
+    /// Return -1 if there are no tokens on channel between
+    /// `token_index` and 0.
+    /// If `token_index` specifies an index at or after the EOF token, the EOF token
+    /// index is returned. This is due to the fact that the EOF token is treated
+    /// as though it were on every channel.
     fn previous_token_on_channel(&self, token_index: isize, channel: isize) -> isize;
 
     fn filter_for_channel(
@@ -22,8 +45,6 @@ pub(crate) trait CommonTokenStreamExt<'input> {
         to: isize,
         channel: isize,
     ) -> Vec<CommonToken<'input>>;
-
-    fn get_tokens(&self) -> Vec<CommonToken<'input>>;
 }
 
 impl<'input> CommonTokenStreamExt<'input> for ActualTokenStream<'input> {
@@ -32,8 +53,11 @@ impl<'input> CommonTokenStreamExt<'input> for ActualTokenStream<'input> {
         token_index: isize,
         channel: isize,
     ) -> Vec<CommonToken<'input>> {
+        // Adapted from <https://github.com/antlr/antlr4/blob/8dcc6526cfb154d688497f31cf1e0904801c6df2/runtime/CSharp/src/BufferedTokenStream.cs#L563>
+
         // This method is private, but it should be alright to leave it out.
         // this.setup();
+
         if token_index < 0 || token_index >= self.size() {
             panic!("{} not in 0..{}", token_index, self.size() - 1);
         }
@@ -48,9 +72,20 @@ impl<'input> CommonTokenStreamExt<'input> for ActualTokenStream<'input> {
         }
     }
 
+    fn get_tokens(&self) -> Vec<CommonToken<'input>> {
+        // Not adapted from anywhere, ANTLR for C# directly exposes this.
+        // Unfortunately, we go and collect the data ourselves.
+        (0..self.size()).map(|i| *self.get(i).clone()).collect()
+    }
+}
+
+impl<'input> PrivateCommonTokenStreamExt<'input> for ActualTokenStream<'input> {
     fn previous_token_on_channel(&self, mut token_index: isize, channel: isize) -> isize {
+        // Adapted from <https://github.com/antlr/antlr4/blob/8dcc6526cfb154d688497f31cf1e0904801c6df2/runtime/CSharp/src/BufferedTokenStream.cs#L488>
+
         // This method is private, but it should be alright to leave it out.
         // this.sync(token_index);
+
         if token_index >= self.size() {
             return self.size() - 1;
         }
@@ -70,6 +105,7 @@ impl<'input> CommonTokenStreamExt<'input> for ActualTokenStream<'input> {
         to: isize,
         channel: isize,
     ) -> Vec<CommonToken<'input>> {
+        // Adapted from <https://github.com/antlr/antlr4/blob/8dcc6526cfb154d688497f31cf1e0904801c6df2/runtime/CSharp/src/BufferedTokenStream.cs#L597>
         let mut token_list = Vec::new();
 
         for index in from..=to {
@@ -84,17 +120,13 @@ impl<'input> CommonTokenStreamExt<'input> for ActualTokenStream<'input> {
         }
         token_list
     }
-
-    fn get_tokens(&self) -> Vec<CommonToken<'input>> {
-        (0..self.size()).map(|i| *self.get(i).clone()).collect()
-    }
 }
 
 pub(crate) fn create_common_token<'a>(
     token_type: isize,
     text: impl Into<String>,
 ) -> Box<CommonToken<'a>> {
-    // Taken from C# implementation of `CommonToken`s constructor
+    // The default values can be found strewn across <https://github.com/antlr/antlr4/blob/8dcc6526cfb154d688497f31cf1e0904801c6df2/runtime/CSharp/src/CommonToken.cs>
     CommonTokenFactory.create::<InputStream<&'a str>>(
         None,
         token_type,
