69b644d941bcdc16d7641666dcb5a74bf5352ca4
Jan Nils Ferner <contact@jnferner.com>
2023-05-02 03:40:48 +0200

    Improve performance

--- crates/compiler/src/compilation_steps/resolve_deferred_type_diagnostic.rs
+++ crates/compiler/src/compilation_steps/resolve_deferred_type_diagnostic.rs
@@ -1,13 +1,16 @@
 use crate::prelude::*;
+use std::collections::HashSet;
 
 pub(crate) fn resolve_deferred_type_diagnostic(
     mut state: CompilationIntermediate,
 ) -> CompilationIntermediate {
+    let known_declarations: HashSet<_> = state
+        .known_variable_declarations
+        .iter()
+        .map(|decl| &decl.name)
+        .collect();
     for deferred_type_diagnostic in &state.potential_issues {
-        let resolved = state
-            .known_variable_declarations
-            .iter()
-            .any(|decl| decl.name == deferred_type_diagnostic.name);
+        let resolved = known_declarations.contains(&deferred_type_diagnostic.name);
 
         if !resolved {
             state
