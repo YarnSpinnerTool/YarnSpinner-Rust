acfcb82bf86383a6b74bf2dffd7bb4f4a2e199e7
Jan Hohenheim <jan@hohenheim.ch>
2023-05-04 02:20:39 +0200

    Fix false positive

--- crates/runtime/tests/test_base/mod.rs
+++ crates/runtime/tests/test_base/mod.rs
@@ -43,6 +43,7 @@ pub fn test_data_path() -> PathBuf {
     .collect()
 }
 
+#[allow(dead_code)] // Allowed because this is a common file and not all tests use this method.
 pub fn space_demo_scripts_path() -> PathBuf {
     let test_data_path = test_data_path();
     let test_data = test_data_path.to_str().unwrap();
