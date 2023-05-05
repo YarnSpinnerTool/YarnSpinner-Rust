f76d243eee8e399fd649485fb64a885227c6581e
Jan Hohenheim <jan@hohenheim.ch>
2023-05-05 21:31:40 +0200

    Add main entrypoint crate

---
+++ crates/yarn_slinger/src/lib.rs
@@ -0,0 +1,8 @@
+pub mod prelude {
+    pub use yarn_slinger_compiler::prelude::*;
+    pub use yarn_slinger_core::generated::*;
+    pub use yarn_slinger_core::prelude::{
+        InvalidCastError, Library, Program, Type, YarnFn, YarnFnRegistry,
+    };
+    pub use yarn_slinger_runtime::prelude::*;
+}
