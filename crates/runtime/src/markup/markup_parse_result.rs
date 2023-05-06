aabaf7a979917909339383db9efa9be8e6d7d383
Jan Hohenheim <jan@hohenheim.ch>
2023-05-06 15:41:42 +0200

    Add derives

--- crates/runtime/src/markup/markup_parse_result.rs
+++ crates/runtime/src/markup/markup_parse_result.rs
@@ -57,6 +57,7 @@ pub(crate) struct MarkupAttribute {
 ///
 /// The original has a discriminator and 4 properties. It's obviously supposed to resemble a discriminated union.
 // TODO: should we use YarnValue here? That one is missing integer, so we currently don't merge them.
+#[derive(Debug, Clone)]
 pub(crate) enum MarkupValue {
     Integer(i32), // TODO: argue about size. In C# float(single) and int(32) are used.
     Float(f32),   // TODO: short is f16, but that doesnt even exist in rust?
@@ -95,7 +96,7 @@ impl MarkupAttributeMarker {
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub(crate) enum TagType {
     /// An open marker. For example, `[a]`.
     Open,
@@ -114,6 +115,7 @@ pub(crate) enum TagType {
 ///
 /// You do not create instances of this struct yourself. It is created
 /// by objects that can parse markup, such as [`Dialogue`]
+#[derive(Debug, Clone)]
 pub(crate) struct MarkupProperty {
     name: String,
     value: MarkupValue,
