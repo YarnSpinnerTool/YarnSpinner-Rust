a1cb198efdb3aeb265e803a890a64e4e90fab91e
Jan Hohenheim <jan@hohenheim.ch>
2023-05-17 00:55:48 +0200

    Improve test file naming

--- crates/bevy_plugin/tests/test_assets.rs
+++ crates/bevy_plugin/tests/test_assets.rs
@@ -10,14 +10,14 @@ fn loads_yarn_assets() {
         .add_plugin(YarnSlingerPlugin);
 
     let asset_server = app.world.get_resource_mut::<AssetServer>().unwrap();
-    let handle = asset_server.load("minimal.yarn");
+    let handle = asset_server.load("lines.yarn");
 
     app.update();
 
     let yarn_file_assets = app.world.get_resource::<Assets<YarnFile>>().unwrap();
     let yarn_file = yarn_file_assets.get(&handle).unwrap();
 
-    let expected_source = include_str!("../assets/minimal.yarn");
+    let expected_source = include_str!("../assets/lines.yarn");
     assert_eq!(expected_source, yarn_file.source);
-    assert_eq!("minimal.yarn", yarn_file.file_name);
+    assert_eq!("lines.yarn", yarn_file.file_name);
 }
