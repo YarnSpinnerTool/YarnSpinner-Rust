04fc07efcca7790c13a39428b1024313a5a96c06
Jan Hohenheim <jan@hohenheim.ch>
2023-05-17 00:51:25 +0200

    Add tests

--- crates/bevy_plugin/src/lib.rs
+++ crates/bevy_plugin/src/lib.rs
@@ -4,4 +4,5 @@ mod plugin;
 pub mod prelude {
     //! Everything you need to get starting using Yarn Slinger.
     pub use crate::plugin::*;
+    pub use yarn_slinger::prelude::YarnFile;
 }
---
+++ crates/bevy_plugin/tests/test_assets.rs
@@ -0,0 +1,24 @@
+use bevy::prelude::*;
+use bevy_yarn_slinger::prelude::*;
+use yarn_slinger::prelude::YarnFile;
+
+#[test]
+fn loads_yarn_assets() {
+    let mut app = App::new();
+
+    // Add Score resource
+    app.add_plugins(DefaultPlugins)
+        .add_plugin(YarnSlingerPlugin);
+
+    let asset_server = app.world.get_resource_mut::<AssetServer>().unwrap();
+    let handle = asset_server.load("minimal.yarn");
+
+    app.update();
+
+    let yarn_file_assets = app.world.get_resource::<Assets<YarnFile>>().unwrap();
+    let yarn_file = yarn_file_assets.get(&handle).unwrap();
+
+    let expected_source = include_str!("../assets/minimal.yarn");
+    assert_eq!(expected_source, yarn_file.source);
+    assert_eq!("minimal.yarn", yarn_file.file_name);
+}
