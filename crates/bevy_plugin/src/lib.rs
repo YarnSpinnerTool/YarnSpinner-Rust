b869491f8e5a5a92bf28960b6233478999b4c973
Jan Hohenheim <jan@hohenheim.ch>
2023-05-17 22:28:43 +0200

    Run cargo fmt

--- crates/bevy_plugin/src/lib.rs
+++ crates/bevy_plugin/src/lib.rs
@@ -4,6 +4,6 @@ mod plugin;
 
 pub mod prelude {
     //! Everything you need to get starting using Yarn Slinger.
-    pub use crate::{plugin::*, config::*};
+    pub use crate::{config::*, plugin::*};
     pub use yarn_slinger::prelude::YarnFile;
 }
