b09a29872c23bf0bda8381e14737393918572937
Jan Nils Ferner <contact@jnferner.com>
2023-05-19 18:22:07 +0200

    Split test in two

--- crates/bevy_plugin/tests/test_assets.rs
+++ crates/bevy_plugin/tests/test_assets.rs
@@ -26,7 +26,7 @@ fn loads_yarn_assets() {
 }
 
 #[test]
-fn generates_localization_files() -> anyhow::Result<()> {
+fn generates_line_ids() -> anyhow::Result<()> {
     let dir = tempdir()?;
     let original_yarn_path = project_root_path().join("assets/lines.yarn");
     let yarn_path = dir.path().join("lines.yarn");
@@ -47,12 +47,12 @@ fn generates_localization_files() -> anyhow::Result<()> {
     let handle = asset_server.load("lines.yarn");
 
     app.update(); // read yarn
-    app.update(); // write line IDs and strings file
-    app.update(); // write updated strings file
+    app.update(); // write line IDs
 
     let yarn_file_assets = app.world.get_resource::<Assets<YarnFile>>().unwrap();
     let yarn_file_in_app = yarn_file_assets.get(&handle).unwrap();
     let yarn_file_on_disk = fs::read_to_string(&yarn_path)?;
+
     assert_eq!(yarn_file_in_app.file.source, yarn_file_on_disk);
     let string_table_without_line_ids = YarnCompiler::new()
         .read_file(&original_yarn_path)
@@ -72,28 +72,59 @@ fn generates_localization_files() -> anyhow::Result<()> {
         string_table_without_line_ids.len(),
         string_table_with_line_ids.len()
     );
+    Ok(())
+}
+
+#[test]
+fn generates_strings_file() -> anyhow::Result<()> {
+    let dir = tempdir()?;
+    let original_yarn_path = project_root_path().join("assets/lines.yarn");
+    let yarn_path = dir.path().join("lines.yarn");
+    fs::copy(&original_yarn_path, &yarn_path)?;
+
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins.set(AssetPlugin {
+        asset_folder: dir.path().to_str().unwrap().to_string(),
+        ..default()
+    }))
+    .add_plugin(YarnSlingerPlugin::with_localizations(Localizations {
+        base_language: "en-US".into(),
+        translations: vec!["de-CH".into()],
+        file_generation_mode: FileGenerationMode::Development,
+    }));
+    let asset_server = app.world.get_resource_mut::<AssetServer>().unwrap();
+    let _handle: Handle<YarnFile> = asset_server.load("lines.yarn");
+
+    app.update(); // read yarn
+    app.update(); // write line IDs and strings file
+    app.update(); // write updated strings file
+
+    let string_table = YarnCompiler::new()
+        .read_file(&yarn_path)
+        .with_compilation_type(CompilationType::StringsOnly)
+        .compile()?
+        .string_table;
 
     assert!(!dir.path().join("en-US.strings.csv").exists());
-    let translation_strings_table_path = dir.path().join("de-CH.strings.csv");
-    assert!(translation_strings_table_path.exists());
-    let translation_strings_table = fs::read_to_string(&translation_strings_table_path)?;
-    let line_ids_in_strings_table: Vec<_> = translation_strings_table
+    let strings_file_path = dir.path().join("de-CH.strings.csv");
+    assert!(strings_file_path.exists());
+    let strings_file_source = fs::read_to_string(&strings_file_path)?;
+    let strings_file_line_ids: Vec<_> = strings_file_source
         .lines()
         .skip(1)
         .map(|line| line.split(',').nth(1).unwrap())
         .collect();
 
-    assert_eq!(
-        string_table_with_line_ids.len(),
-        line_ids_in_strings_table.len()
-    );
+    assert_eq!(string_table.len(), strings_file_line_ids.len());
 
-    assert!(line_ids_in_strings_table
+    assert!(strings_file_line_ids
         .iter()
-        .all(|line_id| string_table_with_line_ids.contains_key(&LineId(line_id.to_string()))));
+        .all(|line_id| string_table.contains_key(&LineId(line_id.to_string()))));
 
     Ok(())
 }
+
 pub fn project_root_path() -> PathBuf {
     PathBuf::from(env!("CARGO_MANIFEST_DIR"))
 }
