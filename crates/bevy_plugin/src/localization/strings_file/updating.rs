cbd3a773ce4fb7fffba870681fd2eb000131a23b
Jan Nils Ferner <contact@jnferner.com>
2023-05-19 14:36:30 +0200

    Write todos

--- crates/bevy_plugin/src/localization/strings_file/updating.rs
+++ crates/bevy_plugin/src/localization/strings_file/updating.rs
@@ -22,17 +22,16 @@ fn update_strings_file_on_yarn_file_change(
             let yarn_file = yarn_files.get(handle).unwrap();
             let mut outdated_unloaded_languages = Vec::new();
             for (language, strings_file_handle) in languages_to_strings_files.0.iter() {
-                // generate new StringsFile
-                // write to disk
+                todo!("generate new StringsFile");
+                todo!("write to disk");
                 if let Some(strings_file) = strings_files.get_mut(strings_file_handle) {
-                    // update mutable strings_file
+                    todo!("update mutable strings_file");
                 } else {
                     outdated_unloaded_languages.push(language.clone());
                 }
             }
             for language in outdated_unloaded_languages {
-                // load again from disk
-                todo!()
+                todo!("load again from disk");
             }
         }
     }
