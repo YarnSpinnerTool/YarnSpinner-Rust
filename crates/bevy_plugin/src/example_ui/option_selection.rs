db9c315bc76ae36db36f4af614b81361db641cad
Jan Hohenheim <jan@hohenheim.ch>
2023-06-05 01:21:41 +0200

    Enable option selection

--- crates/bevy_plugin/src/example_ui/option_selection.rs
+++ crates/bevy_plugin/src/example_ui/option_selection.rs
@@ -1,14 +1,16 @@
-use crate::example_ui::setup::{spawn_options, OptionsNode};
+use crate::example_ui::setup::{spawn_options, DialogueNode, OptionButton, OptionsNode};
 use crate::example_ui::typewriter::Typewriter;
 use crate::example_ui::updating::SpeakerChangeEvent;
-use crate::prelude::DialogueOption;
+use crate::prelude::{DialogueOption, DialogueRunner};
 use bevy::prelude::*;
 
 pub(crate) fn option_selection_plugin(app: &mut App) {
-    app.add_system(
+    app.add_systems((
         show_options
             .run_if(resource_exists::<Typewriter>().and_then(resource_exists::<OptionSelection>())),
-    );
+        select_option
+            .run_if(resource_exists::<Typewriter>().and_then(resource_exists::<OptionSelection>())),
+    ));
 }
 
 #[derive(Debug, Clone, PartialEq, Default, Resource)]
@@ -46,6 +48,44 @@ fn show_options(
                 character_name: name.clone(),
                 speaking: false,
             });
+        }
+    }
+}
+
+fn select_option(
+    typewriter: Res<Typewriter>,
+    mut commands: Commands,
+    mut buttons: Query<
+        (&Interaction, &OptionButton, &mut BackgroundColor),
+        (With<Button>, Changed<Interaction>),
+    >,
+    mut dialogue_runners: Query<&mut DialogueRunner>,
+    mut options_node: Query<(Entity, &mut Style, &mut Visibility), With<OptionsNode>>,
+    mut text: Query<&mut Text, With<DialogueNode>>,
+) {
+    if !typewriter.is_finished() {
+        return;
+    }
+    for (interaction, button, mut color) in buttons.iter_mut() {
+        match *interaction {
+            Interaction::Clicked => {
+                for mut dialogue_runner in dialogue_runners.iter_mut() {
+                    dialogue_runner.select_option(button.0).unwrap();
+                }
+                commands.remove_resource::<OptionSelection>();
+                *color = Color::NONE.into();
+                let (entity, mut style, mut visibility) = options_node.single_mut();
+                commands.entity(entity).despawn_descendants();
+                style.display = Display::None;
+                *visibility = Visibility::Hidden;
+                *text.single_mut() = Text::default();
+            }
+            Interaction::Hovered => {
+                *color = Color::ALICE_BLUE.into();
+            }
+            Interaction::None => {
+                *color = Color::NONE.into();
+            }
         }
     }
 }
