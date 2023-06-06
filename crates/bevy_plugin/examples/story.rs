387d06a331643eb6e12f151047760ce9da17dcb6
Jan Hohenheim <jan@hohenheim.ch>
2023-06-06 03:15:45 +0200

    Load sprites

--- crates/bevy_plugin/examples/story.rs
+++ crates/bevy_plugin/examples/story.rs
@@ -1,7 +1,9 @@
+use bevy::asset::LoadState;
 use bevy::pbr::CascadeShadowConfigBuilder;
 use bevy::prelude::*;
 use bevy::window::PresentMode;
 use bevy_editor_pls::EditorPlugin;
+use bevy_sprite3d::{Sprite3d, Sprite3dParams, Sprite3dPlugin};
 use bevy_yarn_slinger::prelude::*;
 
 #[cfg(not(feature = "example_ui"))]
@@ -37,21 +39,23 @@ fn main() {
         }),
     )
     .add_plugin(ExampleYarnSlingerUiPlugin::new())
+    .add_plugin(Sprite3dPlugin)
     .add_systems((
         setup.on_startup(),
         spawn_dialogue_runner.run_if(resource_added::<YarnProject>()),
+        spawn_sprites.run_if(sprites_have_loaded),
     ))
     .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-1.7, 1.4, 1.8)
-            .looking_at(Vec3::new(-1.5, 1.0, 0.8), Vec3::Y),
+        transform: Transform::from_translation(CAMERA_TRANSLATION)
+            .looking_at(FERRIS_TRANSLATION, Vec3::Y),
         ..default()
     });
     commands.spawn(SceneBundle {
-        scene: asset_server.load("coffee_shop.glb#Scene0"),
+        scene: asset_server.load("models/coffee_shop.glb#Scene0"),
         ..default()
     });
     commands.spawn(DirectionalLightBundle {
@@ -74,6 +78,9 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         color: Color::rgb(1., 0.75, 0.7),
         brightness: 0.25,
     });
+    commands.insert_resource(Sprites {
+        ferris_neutral: asset_server.load("sprites/ferris_neutral.png"),
+    });
 }
 
 fn spawn_dialogue_runner(mut commands: Commands, project: Res<YarnProject>) {
@@ -81,4 +88,40 @@ fn spawn_dialogue_runner(mut commands: Commands, project: Res<YarnProject>) {
     // Immediately start showing the dialogue
     dialogue_runner.start();
     commands.spawn(dialogue_runner);
+}
+
+fn spawn_sprites(
+    mut commands: Commands,
+    sprites: Res<Sprites>,
+    mut sprite_params: Sprite3dParams,
+    mut done: Local<bool>,
+) {
+    if *done {
+        return;
+    }
+    commands.spawn(
+        Sprite3d {
+            image: sprites.ferris_neutral.clone(),
+            pixels_per_metre: 600.,
+            partial_alpha: true,
+            unlit: true,
+            transform: Transform::from_translation(FERRIS_TRANSLATION)
+                .looking_at(CAMERA_TRANSLATION, Vec3::Y),
+            ..default()
+        }
+        .bundle(&mut sprite_params),
+    );
+    *done = true;
+}
+
+const FERRIS_TRANSLATION: Vec3 = Vec3::new(-1.3, 0.9, 0.3);
+const CAMERA_TRANSLATION: Vec3 = Vec3::new(-1.7, 1.4, 1.8);
+
+#[derive(Resource)]
+struct Sprites {
+    ferris_neutral: Handle<Image>,
+}
+
+fn sprites_have_loaded(sprites: Res<Sprites>, asset_server: Res<AssetServer>) -> bool {
+    asset_server.get_load_state(&sprites.ferris_neutral) == LoadState::Loaded
 }
