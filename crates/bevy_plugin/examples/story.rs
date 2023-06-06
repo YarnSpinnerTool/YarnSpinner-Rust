0f141981f279694190b52bb139ea0e226aaeae9a
Jan Hohenheim <jan@hohenheim.ch>
2023-06-06 02:07:24 +0200

    Setup scene

--- crates/bevy_plugin/examples/story.rs
+++ crates/bevy_plugin/examples/story.rs
@@ -1,5 +1,7 @@
+use bevy::pbr::CascadeShadowConfigBuilder;
 use bevy::prelude::*;
 use bevy::window::PresentMode;
+use bevy_editor_pls::EditorPlugin;
 use bevy_yarn_slinger::prelude::*;
 
 #[cfg(not(feature = "example_ui"))]
@@ -26,6 +28,7 @@ fn main() {
                 ..default()
             }),
     )
+    .add_plugin(EditorPlugin::new())
     .add_plugin(
         YarnSlingerPlugin::with_yarn_files(vec!["story.yarn"]).with_localizations(Localizations {
             base_localization: "en-US".into(),
@@ -41,8 +44,36 @@ fn main() {
     .run();
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-1.7, 1.3, 1.8)
+            .looking_at(Vec3::new(-1.5, 1.0, 0.8), Vec3::Y),
+        ..default()
+    });
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("coffee_shop.glb#Scene0"),
+        ..default()
+    });
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            color: Color::BISQUE,
+            illuminance: 10000.,
+            shadows_enabled: true,
+            ..default()
+        },
+        cascade_shadow_config: CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 4.0,
+            maximum_distance: 10.0,
+            ..default()
+        }
+        .into(),
+        transform: Transform::from_xyz(-3., 10., 3.).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+    commands.insert_resource(AmbientLight {
+        color: Color::rgb(1., 0.75, 0.7),
+        brightness: 0.25,
+    });
 }
 
 fn spawn_dialogue_runner(mut commands: Commands, project: Res<YarnProject>) {
