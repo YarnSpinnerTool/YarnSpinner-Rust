8476501898cfa19376668a9f778c24ded6758bf7
Jan Hohenheim <jan@hohenheim.ch>
2023-06-07 02:06:20 +0200

    Write dialogue

--- demo/src/visual_effects.rs
+++ demo/src/visual_effects.rs
@@ -11,7 +11,7 @@ pub(crate) fn bob_speaker(mut speakers: Query<(&Speaker, &mut Transform)>) {
             continue;
         }
         transform.translation.y = speaker.initial_translation.y
-            + (speaker.last_active.elapsed().as_secs_f32() * 12.0)
+            + (speaker.last_active.elapsed().as_secs_f32() * 10.0)
                 .sin()
                 .powi(2)
                 * 0.04;
