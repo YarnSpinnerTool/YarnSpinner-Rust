a0dc4d8eef1cfaaab11194f947261e9aa75a56b7
Jan Hohenheim <jan@hohenheim.ch>
2023-06-10 21:30:39 +0200

    Add readme

--- demo/wasm/run.sh
+++ demo/wasm/run.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# Run this from the repo root.
 # Note that the provided `assets` symlink only works on macOS and Linux.
 # On Windows, you'll have to remove it and then copy the directory `crates/bevy_plugin/assets` to `crates/bevy_plugin/wasm/assets` manually.
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR"/../..
 
 rustup target add wasm32-unknown-unknown
 cargo build --bin bevy_yarn_slinger_demo --target wasm32-unknown-unknown
