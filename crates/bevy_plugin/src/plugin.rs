3937a928ce50d39181857a50bd4a3465604112dd
Tim Jentzsch <commits@timjen.net>
2023-08-03 19:37:43 +0200

    Adapt to new asset change watcher

--- crates/bevy_plugin/src/plugin.rs
+++ crates/bevy_plugin/src/plugin.rs
@@ -237,6 +237,7 @@ impl YarnApp for App {
         let asset_plugins: Vec<&AssetPlugin> = self.get_added_plugins();
         let asset_plugin: &AssetPlugin = asset_plugins.into_iter().next().expect("Yarn Slinger requires access to the Bevy asset plugin. \
         Please add `YarnSlingerPlugin` after `AssetPlugin`, which is commonly added as part of the `DefaultPlugins`");
-        asset_plugin.watch_for_changes
+
+        asset_plugin.watch_for_changes.is_some()
     }
 }
