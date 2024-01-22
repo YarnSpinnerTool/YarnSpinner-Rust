fc8887c86541a6a492dbb254e40c2cb1bbd48ee6
Jan Hohenheim <jan@hohenheim.ch>
2024-01-23 00:25:04 +0100

    Force file watcher on supported systems

--- crates/bevy_plugin/src/plugin.rs
+++ crates/bevy_plugin/src/plugin.rs
@@ -234,10 +234,6 @@ impl YarnApp for App {
     }
 
     fn is_watching_for_changes(&self) -> bool {
-        let asset_plugins: Vec<&AssetPlugin> = self.get_added_plugins();
-        let asset_plugin: &AssetPlugin = asset_plugins.into_iter().next().expect("Yarn Slinger requires access to the Bevy asset plugin. \
-        Please add `YarnSlingerPlugin` after `AssetPlugin`, which is commonly added as part of the `DefaultPlugins`");
-
-        asset_plugin.watch_for_changes.is_some()
+        cfg!(all(not(target_arch = "wasm32"), not(target_os = "android")))
     }
 }
