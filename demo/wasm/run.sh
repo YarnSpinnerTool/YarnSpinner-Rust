0d191ad3dec171c68f69c46c1cd9246a2f3e1254
Jan Hohenheim <jan@hohenheim.ch>
2024-01-23 22:22:34 +0100

    Update wasm script

--- demo/wasm/run.sh
+++ demo/wasm/run.sh
@@ -9,10 +9,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"/../..
 
 rustup target add wasm32-unknown-unknown
-cargo build --bin bevy_yarn_slinger_demo --features editor --target wasm32-unknown-unknown
+cargo build --bin bevy_yarn_slinger_demo --target wasm32-unknown-unknown
 
 # Keep this in sync with the version in `Cargo.lock`.
-cargo install wasm-bindgen-cli --version 0.2.87 || true
+cargo install wasm-bindgen-cli --version 0.2.90 || true
 wasm-bindgen --no-typescript --out-name bevy_yarn_slinger_demo --out-dir demo/wasm --target web target/wasm32-unknown-unknown/debug/bevy_yarn_slinger_demo.wasm
 
 cargo install basic-http-server || true
