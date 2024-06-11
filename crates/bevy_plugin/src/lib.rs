7e10c1a0394d85a1f015da11bdc8e4c423a175c1
Jan Hohenheim <jan@hohenheim.ch>
2024-06-11 18:11:35 +0200

    Move readme doctest

--- crates/bevy_plugin/src/lib.rs
+++ crates/bevy_plugin/src/lib.rs
@@ -167,7 +167,3 @@ pub mod deferred_loading {
     pub use crate::plugin::DeferredYarnSpinnerPlugin;
     pub use crate::project::LoadYarnProjectEvent;
 }
-
-#[cfg(doctest)]
-#[doc = include_str!("../../../readme.md")]
-mod test_readme {}
