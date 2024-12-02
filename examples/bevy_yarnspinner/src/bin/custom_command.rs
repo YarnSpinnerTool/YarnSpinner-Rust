4e2324be9de2ce27a553a854872edea3cac8b552
Cian Ormond <co@cianormond.com>
2024-12-01 20:34:23 -0800

    Update examples to Bevy 0.15 and fix test

--- examples/bevy_yarnspinner/src/bin/custom_command.rs
+++ examples/bevy_yarnspinner/src/bin/custom_command.rs
@@ -19,7 +19,7 @@ fn main() {
 }
 
 fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2d::default());
 }
 
 fn spawn_dialogue_runner(mut commands: Commands, project: Res<YarnProject>) {
