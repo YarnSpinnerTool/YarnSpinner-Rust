cb8abd362e47bdca7e5735f684e6e5114845db7f
Rex Magana <Rex.magana@gmail.com>
2025-04-26 07:23:17 -0700

    forgot vec dequeueue

--- crates/runtime/src/markup/line_parser.rs
+++ crates/runtime/src/markup/line_parser.rs
@@ -1,6 +1,6 @@
 //! Adapted from <https://github.com/YarnSpinnerTool/YarnSpinner/blob/da39c7195107d8211f21c263e4084f773b84eaff/YarnSpinner/YarnSpinner.Markup/LineParser.cs>
 
-use std::collections::VecDeque;
+use alloc::collections::VecDeque;
 
 use crate::markup::parsed_markup::ParsedMarkup;
 use crate::markup::{
