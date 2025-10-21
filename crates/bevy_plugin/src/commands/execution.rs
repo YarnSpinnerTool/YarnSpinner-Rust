49df236081d505839417bd75a9782608289a82b0
Glenn Macdonald <jebus.422@gmail.com>
2025-10-21 16:42:29 -0400

    clean up execute_commands function

--- crates/bevy_plugin/src/commands/execution.rs
+++ crates/bevy_plugin/src/commands/execution.rs
@@ -9,33 +9,40 @@ pub(crate) fn command_execution_plugin(app: &mut App) {
 
 fn execute_commands(
     event: On<ExecuteCommand>,
-    dialogue_runners: Query<&DialogueRunner>,
     mut commands: Commands,
 ) {
-    let dialogue_runner = dialogue_runners.get(event.entity).unwrap();
-    let Some(mut command) = clone_command(dialogue_runner, &event) else {
-        return;
-    };
-    let params = event.command.parameters.clone();
-    let entity = event.entity;
+    let event = event.clone();
     commands.queue(move |world: &mut World| {
-        let task_finished_indicator = command.call(params.clone(), world);
+        let Some(mut command) = clone_command(world, &event) else {
+            return;
+        };
+
+        let params = event.command.parameters;
+        let task_finished_indicator = command.call(params, world);
         if !task_finished_indicator.is_finished() {
-            get_dialogue_runner_mut(world, entity).add_command_task(task_finished_indicator);
+            get_dialogue_runner_mut(world, event.entity).add_command_task(task_finished_indicator);
         }
     });
 }
 
 fn clone_command(
-    dialogue_runner: &DialogueRunner,
+    world: &mut World,
     event: &ExecuteCommand,
 ) -> Option<Box<dyn UntypedYarnCommand>> {
+    let dialogue_runner = get_dialogue_runner(world, event.entity);
     let command_name = event.command.name.as_str();
     dialogue_runner
         .commands
         .get(command_name)
         .map(|command| command.clone_box())
 }
+
+fn get_dialogue_runner(world: &mut World, entity: Entity) -> &DialogueRunner {
+    let mut dialogue_runners = world.query::<&DialogueRunner>();
+
+    (dialogue_runners.get(world, entity).unwrap()) as _
+}
+
 
 fn get_dialogue_runner_mut(world: &mut World, entity: Entity) -> Mut<'_, DialogueRunner> {
     let mut dialogue_runners = world.query::<&mut DialogueRunner>();
