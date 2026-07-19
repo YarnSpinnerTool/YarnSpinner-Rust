569f0ff179507fe999973bce2033754ad23c9cb3
Finix <git@littlefinix.net>
2026-07-19 20:20:50 +0200

    change `get_hashable_interval` in hashable_interval.rs to work around bug

--- crates/compiler/src/visitors/hashable_interval.rs
+++ crates/compiler/src/visitors/hashable_interval.rs
@@ -98,6 +98,8 @@ impl DerefMut for KnownTypes {
 
 pub(crate) trait GetHashableInterval<'input>: ParserRuleContext<'input> {
     fn get_hashable_interval(&self) -> HashableInterval {
+        // FIXME: The get_source_interval function only seems to return INVALID values.
+        //        I'm not sure if this is a bug in antlr4rust of an issue with the types in this function.
         let interval = Interval {
             a: self.start().get_token_index() as i32,
             b: self.stop().get_token_index() as i32,
